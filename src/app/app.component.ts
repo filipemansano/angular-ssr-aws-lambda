dc4ae724a5434e64d3bb4ca4f15a7da8621ecaaf
filipe mansano <filipemansano@gmail.com>
2023-02-09 01:54:18 -0300

    add aws lambda support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-ssr';
+export class AppComponent implements OnInit {
+  
+  data$!: Observable<any[]>;
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    this.data$ = this.http.get<any[]>('https://api.publicapis.org/entries').pipe(
+      map((data:any) => data.entries)
+    );
+  }
 }
