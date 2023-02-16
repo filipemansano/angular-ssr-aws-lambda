cb765188eb35bb79e91589915024fb7126d60bdd
filipe mansano <filipemansano@gmail.com>
2023-02-15 22:19:52 -0300

    new lambda

--- dist/angular-ssr/lambda.js
+++ dist/angular-ssr/lambda.js
@@ -1,8 +1,4 @@
 const serverlessExpress = require('@vendia/serverless-express')
-const serverlessExpressMiddleware = require('@vendia/serverless-express/middleware');
 const server = require('./server/main');
-
 const app = server.app();
-app.use(serverlessExpressMiddleware.eventContext());
-
 exports.handler = serverlessExpress({ app })
